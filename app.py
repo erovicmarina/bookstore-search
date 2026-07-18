28d58fa14c371a7d9190bdcbf5554e1bdd1e20d7
Marina <marinaerovic28@gmail.com>
2026-07-18 11:54:31 +0200

    Connected to real Notion data source and cleaned up book data

--- app.py
+++ app.py
@@ -1,10 +1,37 @@
-from flask import Flask
+from flask import Flask, jsonify
+from notion_client import Client
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
-@app.route("/") 
+notion = Client(auth=os.environ["NOTION_TOKEN"])
+datasource_id = os.environ["NOTION_DATASOURCE_ID"]
 
+@app.route("/") 
 def home():
-    return "Svako dobro dobri ljudi"
+    return "Book club backend running"
+
+def clean_book(page):
+    props = page["properties"]
+    naziv = props["NAZIV KNJIGE"]["title"][0]["plain_text"] if props["NAZIV KNJIGE"]["title"] else "Bez naslova"
+    autor = props["AUTOR"]["rich_text"][0]["plain_text"] if props["AUTOR"]["rich_text"] else "Nepoznat autor"
+    status = props["STATUS"]["select"]["name"] if props["STATUS"]["select"] else "NEPOZNATO"
+    zanr = [tag["name"] for tag in props["ZANR"]["multi_select"]]
+    godina = props["GODINA IZDANJA"]["number"]
+    return { 
+        "naziv" : naziv,
+        "autor" : autor,
+        "status": status,
+        "zanr": zanr,
+        "godina": godina
+    }
+@app.route("/api/books")
+def get_books():
+    response = notion.data_sources.query(data_source_id=datasource_id)
+    books = [clean_book(page) for page in response["results"]]
+    return jsonify(books)
 
 if __name__ == "__main__":
     app.run(debug=True)
